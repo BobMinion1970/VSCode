a746e94ca80f9a07239a92e3cdcbe13e3d072adf
Bob Minion <bob.minion1970@gmx.de>
2023-05-04 21:54:12 +0200

    Event Handler added

--- Python/FirstGridGUI/main.py
+++ Python/FirstGridGUI/main.py
@@ -1,27 +1,38 @@
 import tkinter
+import tkinter.font
 
 ######################################### Classes ##################################
 class FirstGridGUI(tkinter.Frame):
 
-    def __init__(self, master=None):
-        tkinter.Frame.__init__(self, master)
+    def __init__(self, parent):
+        #tkinter.Frame.__init__(self)
+        super().__init__(parent)
         self.createWidgets()
+    
+    def onLabelClick(self, event):
+        if event.widget["text"] == "1":
+            self.lblGray.configure(bg="blue")
 
+        if self.lblGray.cget("bg") == "gray":
+            self.lblGray.configure(bg="red")
+
+        
     def createWidgets(self):
        
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(1 ,weight=1)
+        self.grid_rowconfigure( 0, weight=1)
+        self.grid_columnconfigure( 0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
         self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(2, weight=1)
         self.grid_columnconfigure(2, weight=1)
-        self.lblGray = tkinter.Label(width=10, height=5, bg='gray')
+        self.lblGray = tkinter.Label(text="1", bg='gray', font='Arial 24 bold')
+        self.lblGray.bind('<Button-1>', self.onLabelClick)
         self.lblGray.grid(row=0, column=0, sticky="NESW")
-        self.lblGreen = tkinter.Label(width=10, height=5, bg='green')
+        self.lblGreen = tkinter.Label(text="2", width=10, height=5, bg='green', font='Arial 24 bold')
         self.lblGreen.grid(row=0, column=1, sticky="NESW")
-        self.lblYellow = tkinter.Label(width=10, height=5, bg='yellow')
+        self.lblYellow = tkinter.Label(text="3", width=10, height=5, bg='yellow', font='Arial 24 bold')
         self.lblYellow.grid(row=0, column=2, sticky='NESW')
-        self.lblBlue = tkinter.Label(width=10, height=5, bg='blue')
+        self.lblBlue = tkinter.Label( width=10, height=5, bg='blue')
         self.lblBlue.grid(row=1, column=0, sticky='NESW')
         self.lblBrown = tkinter.Label(width=10, height=5, bg='brown')
         self.lblBrown.grid(row=1, column=1, sticky='NESW')
@@ -38,8 +49,11 @@ class FirstGridGUI(tkinter.Frame):
 def main():
     root = tkinter.Tk(screenName="FirstGridGUI")
     root.resizable(True, True)
+    root.grid_rowconfigure(0, weight=1)
+    root.grid_columnconfigure(0, weight=1)
     app = FirstGridGUI(root)
-    # app.createWidgets()
+    app.grid(row=0, column=0, sticky='NESW')
+    
     app.mainloop()
 
 ########################################### Main #####################################
