542d2b335a6c3d43a5684001c89219cdd5a3375a
Bob Minion <bob.minion1970@gmx.de>
2023-05-04 10:42:06 +0200

    final version.

--- Python/WeightWatchersGUI/main.py
+++ Python/WeightWatchersGUI/main.py
@@ -1,39 +1,43 @@
-import tkinter
+import tkinter 
+import tkinter.messagebox
 
+#################################### Classes ##################################################
 class myProgram(tkinter.Frame):
     def __init__(self, master=None):
         tkinter.Frame.__init__(self, master)
         self.pack()
         self.createWidgets()
-    
-    def onCalculate():
-        return 0
 
+    def onCalculate(self):
+        result=float(self.lblInputJoule.get()) * 0.24
+        self.lblKalorien["text"] = str(result)
+        return 
+    
+    def onExit(self):
+        tkinter.messagebox.showinfo(title = "MyProgram", message = "App will be closed", options=None)
+        self.quit()
+     
     def createWidgets(self):
-        self.lblJoule = tkinter.Label(self) 
-        self.lblJoule["text"] = "Please provide kJoule: "
+        self.lblJoule = tkinter.Label(self, text="Please provide kJoule: ") 
         self.lblJoule.pack()
         self.lblInputJoule = tkinter.Entry(self)
-        self.lblInputJoule.pack(side="left")
+        self.lblInputJoule.pack()
         self.lblInputJoule["width"] = 10
-        self.strJoule = tkinter.StringVar()
-        self.lblInputJoule["textvariable"] = self.strJoule
-
-        self.btnExit = tkinter.Button(self)
-        self.btnExit[text] = "Exit"
-        self.btnExit[commmand] = self.quit
-        self.btnExit.
-        self.btnExit.pack(side="right")
-
-        self.btnCalc = tkinter.Button(self)
-        self.btnCalc[text] = "Calculate"
-        self.btnCalc[command] = self.onCalculate
-
+        self.btnExit = tkinter.Button(self, text = "Exit", command=self.onExit)
+        #self.btnExit["text"] = "Exit"
+        #self.btnExit["commmand"] = self.quit
+        self.btnExit.pack()
         
+        self.btnCalc = tkinter.Button(self, text="Calculate", command=self.onCalculate)
+        self.btnCalc.pack()
+        self.lblKalorien = tkinter.Label(self)
+        self.lblKalorien.pack()  
 
-
+#################################### Functions ##################################################
 def main():
-    root = tkinter.Tk()
+    root = tkinter.Tk(screenName="WeightWatcher")
+    root.title("Weight Watcher's Fun")
+    root.geometry('440x200')
     app = myProgram(root)
     app.mainloop()
 
