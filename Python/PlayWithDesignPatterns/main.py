e34cc8d7f25b05959c960db0e759b5a1e4cdd523
Bob Minion <bob.minion1970@gmx.de>
2023-05-20 14:22:34 +0200

    Initialize project PlayWithDesignPatterns

---
+++ Python/PlayWithDesignPatterns/main.py
@@ -0,0 +1,22 @@
+####################################### Functions ###################################
+import datetime
+
+def test_decorator():
+    myfunc()
+
+def logger(func):
+    def wrapper():
+        print(str(datetime.datetime.now()) + " --- Function call started: " + str(func))
+        func()
+        print(str(datetime.datetime.now()) + " --- Function call finished: " + str(func))
+    return wrapper
+
+@logger
+def myfunc():
+    print("Inside myfunc")
+    
+def main():
+    test_decorator()
+    
+####################################### Main ########################################
+main()
