e28b4fe4b5b8843600aef05b55b902c5c066a703
Bob Minion <bob.minion1970@gmx.de>
2023-07-02 17:55:05 +0200

    Adding docstring to to module Archer.py

--- Python/PlayWithClasses/Classes/Archer.py
+++ Python/PlayWithClasses/Classes/Archer.py
@@ -7,11 +7,14 @@ import json
 from .DataArcher import DataArcher
 from Interfaces.IArcher import IArcher
 
-# class ArrowType(StrEnum):
-#     L = "Long bow"
-#     S = "Short bow"
+
 
 class Archer(DataArcher, IArcher):
+    """ Archer, which can be hit and shoot arrows
+    Arguments:
+        DataArcher -- sub class containing all member variables
+        IArcher -- interface for all archers 
+    """
     def __init__(self, myid: int, hitpoints: int, arrows: int):
         super().__init__(myid, hitpoints, arrows)
         # _id = id
@@ -22,6 +25,11 @@ class Archer(DataArcher, IArcher):
         return f"Archer ID: {str(self._myid)}, hitpoints: {str(self._hitpoints)}, arrows: {str(self._arrows)}"
     
     def shoot(self):
+        """ Name: shoot
+        Description: shoots an arrow and decrements the number of arrows
+        Raises:
+            ValueError: if number of arrows is 0 
+        """
         try:
             if self._arrows >= 1:
                 self._arrows -= 1
@@ -29,10 +37,20 @@ class Archer(DataArcher, IArcher):
                 raise ValueError
         except RuntimeError as e: 
             print(e.__str__())
+    
         
     def increase_arrows(self, new_arrows:int):
+        """ Name: increase_arrows
+        Description: increases the number of arrows of an instance
+        Arguments:
+            new_arrows -- number of new arrows
+        """
         self._arrows += new_arrows
         
     def toJson(self):
+        """ exports the instance dictionary to a json format
+        Returns:
+            json dump of instance dictionary in format keyword = value
+        """
         return json.dumps(self, default=lambda o: o.__dict__)
         
