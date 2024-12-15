b10d8f0a74a4228fd952372a59207013e920bd6d
unknown <bob.minion1970@gmx.de>
2024-12-15 17:58:44 +0100

    Added command line args plus json input file

--- Python/PythonCrashCourse/PythonCrashCourseMain.py
+++ Python/PythonCrashCourse/PythonCrashCourseMain.py
@@ -6,9 +6,45 @@ from src.interfaces.IAccount import IAccount
 from src.interfaces.ICustomer import ICustomer
 from src.classes.Customer import Customer
 from src.classes.Account import Account
-
+import argparse
+import json
+import os
+import collections
 
 def main() -> None:
+    ############# region for command line arguments ###############
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i','--input', dest='infile')
+    args = parser.parse_args()
+    if args.infile:
+        print("Command line argument for input file: " + args.infile)
+    
+    print("Current folder: " + os.getcwd())
+    ############ region where json data comes in ##################
+    customers = {}
+    with open("Customers.json", mode="r", encoding="utf-8") as input_file:
+        try: 
+            customers = json.load(input_file)
+            print ("Type customers: " + str(type(customers)))
+            
+            customerlist = customers["Customers"]
+            print ("Type customerlist: " + str(type(customerlist)))
+           
+            searchfirstname = input("Which name should be searched: ")
+                           
+            #print(customers)
+            for index in range(len(customerlist)):
+                print("Type of one customer: " + str(type(customerlist[index])))
+                if customerlist[index]["firstname"] == searchfirstname:
+                    print(customerlist[index])
+                
+           
+        except FileNotFoundError:
+            print(f"File not found: {input_file}")
+        except Exception:
+            print(f"{Exception}")
+    
+    ############ region for object oriented style #################
     account1 : IAccount = None
     account2 : IAccount = None
     customer1: ICustomer = None
@@ -43,8 +79,6 @@ def main() -> None:
     except ArithmeticError: 
         print(f"Credit is too low: {customer2.account.credit} Please reduce the required withdrawal: 2000")
 
-
+    
 ############################################### Main ###########################################
 main()
-
-
